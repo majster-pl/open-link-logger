f23279511d8a75a31ea2488080bc282c22ced73b
majster-pl <waliczek.szymon@gmail.com>
2022-02-02 00:46:30 +0000

    Fix of axios request and error on refresh page other then root /

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ console.dir(data_path);
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   //   res.sendFile("./views/index.html", { root: __dirname });
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
